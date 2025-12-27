a136972c0a32c01223cf0658d852238e88c3ed3f
anbumk999 <anbumk999@gmail.com>
2025-12-27 13:33:46 +0530

    LongestSubstring

---
+++ src/problem/Factorial.java
@@ -0,0 +1,17 @@
+package problem;
+
+public class Factorial {
+
+   public static void main(String[] args) {
+
+       int result = 1;
+
+       for(int j = 1 ; j <=5 ; j++){
+
+           result = result * j ;
+
+       }
+
+       System.out.println(result);
+    }
+}
