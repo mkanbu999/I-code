f1cc6cc766db47ea3f56cde26b9ac71dba4a7f1a
anbumk999 <anbumk999@gmail.com>
2026-01-04 21:20:59 +0530

    LongestSubstring

---
+++ src/array/GroupAnagran.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.*;
+
+public class GroupAnagran {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(input));
+    }
+}
